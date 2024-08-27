8d852921430bbae6eb354e61e8317c07cf0b43b4
INTEL-ONBOARD <75471327+INTEL-ONBOARD@users.noreply.github.com>
2024-08-27 09:25:09 +0530

    first commit

---
+++ LMS/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home'; 
+import About from './pages/About';
+import Contact from './pages/Contact';
+import SignIn from './pages/SignIn';
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/signin" element={<SignIn />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
