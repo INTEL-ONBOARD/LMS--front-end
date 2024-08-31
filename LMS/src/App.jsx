edee5cd2f5b337d30f0da0e13724f687d6ed1199
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-08-31 20:01:28 +0530

    Small Configurations

--- LMS/src/App.jsx
+++ LMS/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home'; 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
     return (
         <Router>
             <div>
-                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/signin" element={<SignIn />} />
+                    <Route path="/dashboard" element={<Dashboard/>}/>
                 </Routes>
             </div>
         </Router>
