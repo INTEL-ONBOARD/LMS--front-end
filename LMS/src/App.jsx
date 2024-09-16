ecdb479e8b604c1e3dcd48c91168d538a6f58cc0
Grandeur77 <chethanarowell2002@gmail.com>
2024-09-17 00:57:38 +0530

    divide signin and signup pages

--- LMS/src/App.jsx
+++ LMS/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
                     <Route path="/dashboard" element={<Dashboard/>}/>
                 </Routes>
             </div>
